0f73fdbe4b2065ce9a0f2ec1487e7bd39c7b991f
Bastien Mourrat <bastien.mourrat@octo.com>
2021-02-08 16:15:25 +0100

    some tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -35,4 +35,49 @@ class GildedRoseTest {
         // Then
         assertEquals(items[0].sellIn, 2);
     }
+
+    @Test
+    void itemIsBackstageOrAgedbrieAndQualityUnder50ThenQualityPlusOne() {
+        // Given
+        Item[] items = new Item[] { new Item("Aged Brie", 3, 39) };
+        GildedRose app = new GildedRose(items);
+        // When
+        app.updateQuality();
+        // Then
+        assertEquals(items[0].quality, 40);
+    }
+
+    @Test
+    void itemIsNotBackstageNorAgedbrieNorSulfurasAndQualityOver0ThenQualityMinusOne() {
+        // Given
+        Item[] items = new Item[] { new Item("Some item", 3, 26) };
+        GildedRose app = new GildedRose(items);
+        // When
+        app.updateQuality();
+        // Then
+        assertEquals(items[0].quality, 25);
+    }
+
+    @Test
+    void sellinNegativeandIsBackstageThenQualityEqualsZero() {
+        // Given
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -3, 26) };
+        GildedRose app = new GildedRose(items);
+        // When
+        app.updateQuality();
+        // Then
+        assertEquals(items[0].quality, 0);
+    }
+
+    @Test
+    void sellinNegativeAndItemAgedBrieAndQualityUnder50() {
+        // Given
+        int quality = 23;
+        Item[] items = new Item[] { new Item("Aged Brie", -3, quality) };
+        GildedRose app = new GildedRose(items);
+        // When
+        app.updateQuality();
+        // Then
+        assertEquals(quality+2, items[0].quality);
+    }
 }
